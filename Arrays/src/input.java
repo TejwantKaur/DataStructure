dfa50111d6d19895cfa9acaf66090260b84103a3
TejwantKaur <kaurtejwant998@gmail.com>
2023-10-27 12:05:36 +0530

    input

--- Arrays/src/input.java
+++ Arrays/src/input.java
@@ -3,6 +3,8 @@ public class input {
     public static void main(String[] args) {
         int[] marks = new int [5];
 
+        System.out.println("Length of array is " + marks.length);
+
         Scanner sc = new Scanner(System.in);
 
         marks[0] = sc.nextInt();
@@ -12,5 +14,11 @@ public class input {
         System.out.println("marks1="+marks[0]);
         System.out.println("marks2="+marks[1]);
         System.out.println("marks3="+marks[2]);
+
+        marks[1] += 1;
+        System.out.println("updated marks2="+marks[1]);
+
+        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        System.out.println("Percentage is " + percentage + "%");
     }
 }
