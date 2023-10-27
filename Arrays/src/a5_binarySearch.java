d98bc72a5942479d0ff3493b67ccf79b36002b6b
TejwantKaur <kaurtejwant998@gmail.com>
2023-10-27 13:46:50 +0530

    binary Search

---
+++ Arrays/src/a5_binarySearch.java
@@ -0,0 +1,34 @@
+public class a5_binarySearch {
+
+    private static int binarySearch(int[] arr, int key){
+
+        int start=0, end=arr.length-1;
+
+        while(start<=end){
+            int midIdx = (start+end)/2;
+
+            if(arr[midIdx] == key){
+                return midIdx;
+            }
+            else if(arr[midIdx]>key){
+                end = midIdx-1;
+            }
+            else{
+                start = midIdx+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] sortedArr = {1,4,6,8,12,14,16,24,35};
+        int key = 8;
+
+        int idx=binarySearch(sortedArr,key);
+        if(idx == -1){
+            System.out.println("Not Found");
+        }else{
+            System.out.println("Idx of key:"+key+ " is "+idx);
+        }
+
+    }
+}
