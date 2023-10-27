0633774b8c36c89f5d879f028a61f8354bb7e1d1
TejwantKaur <kaurtejwant998@gmail.com>
2023-10-27 13:15:23 +0530

    simple menu

---
+++ Arrays/src/a11_activity.java
@@ -0,0 +1,23 @@
+public class a11_activity {
+
+    public static int find(String arr[], String key){
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].equals(key)){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String[] menu = {"samosa", "pasta", "dosa"};
+        String key = "pasta";
+        String key2 = "dosa";
+
+        System.out.println(find(menu,key));
+        System.out.println(find(menu,key2));
+
+    }
+}
