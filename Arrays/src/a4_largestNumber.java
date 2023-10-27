adf4a73ad61efd103901866b1804792ca3e34025
TejwantKaur <kaurtejwant998@gmail.com>
2023-10-27 13:29:54 +0530

    find largest and Smallest number

---
+++ Arrays/src/a4_largestNumber.java
@@ -0,0 +1,27 @@
+public class a4_largestNumber {
+    private static int findLargest(int[] arr) {
+        int largest=Integer.MIN_VALUE;
+
+        for(int num:arr){
+            if(num>largest){
+                largest=num;
+            }
+        }
+        return largest;
+    }
+    private static int findSmallest(int[] arr) {
+        int Smallest=Integer.MAX_VALUE;
+
+        for(int num:arr){
+            if(num<Smallest){
+                Smallest=num;
+            }
+        }
+        return Smallest;
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,6,8,12,89,13,4,1};
+        System.out.println(findLargest(arr));
+        System.out.println(findSmallest(arr));
+    }
+}
